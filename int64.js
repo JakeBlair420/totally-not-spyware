a85b3fed8f4abbc1664adf251b8006e64a3a6d03
Ben Sparkes <bensparkes8@gmail.com>
2018-06-21 00:01:20 +0100

    move int64:::hi/lo

--- int64.js
+++ int64.js
@@ -132,6 +132,18 @@ Int64.fromDouble = function(d) {
     return new Int64(bytes);
 };
 
+Int64.prototype.lo = function()
+{
+    var b = this.bytes();
+    return (b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)) >>> 0;
+};
+
+Int64.prototype.hi = function()
+{
+    var b = this.bytes();
+    return (b[4] | (b[5] << 8) | (b[6] << 16) | (b[7] << 24)) >>> 0;
+};
+
 // Convenience functions. These allocate a new Int64 to hold the result.
 
 // Return -n (two's complement)
