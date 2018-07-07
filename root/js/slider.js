3e8e7af9988aae94be8317fbb078e6e39488ba4b
Viktor Oreshkin <imselfish@stek29.rocks>
2018-07-07 15:42:17 +0300

    [UI] Add slide for unlock text

--- root/js/slider.js
+++ root/js/slider.js
@@ -1,37 +1,51 @@
 // slider.js - slider logic from JailbreakMe (Star)
 
-// Get slider's thumbtack object
 var thumbtack = document.getElementById('thumbtack');
+var hint = document.getElementById('hint');
 
 // Set spacing by margin (hack)
 var left = 0;
+var startX = null;
+var maxLeft = thumbtack.parentNode.clientWidth - thumbtack.clientWidth - 5;
+var startLeft = null;
+
 function set_left(left_) {
+    console.log("set_left")
     left = left_;
+    hint.style.opacity = 1 - (left_ / (maxLeft * 1/4));
     thumbtack.style.marginLeft = left_ + 'px';
 }
 
-thumbtack.ontouchstart = function(e) {
-    startX = e.targetTouches[0].clientX;
+var onDown = function(x) {
+    startX = x;
     startLeft = left;
     thumbtack.style.WebkitTransitionProperty = '';
     thumbtack.style.WebkitTransitionDuration = '0s';
-    maxLeft = thumbtack.parentNode.clientWidth - thumbtack.clientWidth - 5;
     return false;
 }
 
-thumbtack.ontouchmove = function(e) {
-    var diff = e.targetTouches[0].clientX - startX;
-    if(diff < 0) diff = 0;
-    else if(diff >= maxLeft) diff = maxLeft;
+var onMove = function(x) {
+    if (startX == null) return;
+
+    var diff = x - startX;
+
+    if (diff < 0) {
+        diff = 0;
+    } else if (diff >= maxLeft) {
+        diff = maxLeft;
+    }
+
     set_left(diff + startLeft);
 }
 
-window.ontouchend = function() {
+var onEnd = function() {
     if(startX == null) return;
     startX = null;
-    if(maxLeft - left < 15) {
+    
+    if (maxLeft - left < 15) {
         return false;
     }
+
     var left_ = left;
     set_left(0);
     thumbtack.style.WebkitTransform = 'translateX('+left_+'px)';
@@ -42,3 +56,11 @@ window.ontouchend = function() {
     }, 0);
     return false;
 }
+
+thumbtack.ontouchstart = e => onDown(e.targetTouches[0].clientX);
+window.ontouchmove = e => onMove(e.targetTouches[0].clientX);
+window.ontouchend = e => onEnd();
+
+thumbtack.onmousedown = e => onDown(e.clientX);
+window.onmousemove = e => onMove(e.clientX);
+window.onmouseup = e => onEnd();
