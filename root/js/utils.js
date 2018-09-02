5364b32016dfd9465e1f5423cc2eef8ee98425f8
Viktor Oreshkin <imselfish@stek29.rocks>
2018-09-02 09:22:00 +0300

    Add option to force compatibility

--- root/js/utils.js
+++ root/js/utils.js
@@ -76,3 +76,17 @@ var Struct = (function() {
         float64: float64View
     };
 })();
+
+
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var bufView = new Uint16Array(buf);
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
+  }
+  return buf;
+}
