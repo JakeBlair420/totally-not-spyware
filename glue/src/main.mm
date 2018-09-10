ef0a30bb1353150854095fadeee3e746fcbaa9d9
Siguza <siguza@siguza.net>
2018-09-10 22:30:22 +0200

    1.15

--- glue/src/main.mm
+++ glue/src/main.mm
@@ -122,7 +122,7 @@ int main(void)
     @autoreleasepool
     {
         LOG("we out here\n");
-        LOG("v1.9\n");
+        LOG("v1.15\n");
 
         tihmstar::offsetfinder64 fi("/System/Library/Caches/com.apple.kernelcaches/kernelcache");
 
